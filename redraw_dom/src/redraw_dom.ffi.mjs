624f0c8bbd0fe29ec0728f8fad08799b06b7c589
Guillaume Hivert <hivert.is.coming@gmail.com>
2024-10-27 11:33:27 +0100

    Use Result(Root, Nil) for failable functions

--- redraw_dom/src/redraw_dom.ffi.mjs
+++ redraw_dom/src/redraw_dom.ffi.mjs
@@ -1,18 +1,22 @@
 import ReactDOM from "react-dom/client"
+import * as gleam from "./gleam.mjs"
 
 export function createRoot(value) {
   const node = document.getElementById(value)
-  return ReactDOM.createRoot(node)
+  if (!node) return new gleam.Error()
+  return new gleam.Ok(ReactDOM.createRoot(node))
 }
 
 export function hydrateRoot(value, content) {
   const node = document.getElementById(value)
-  return ReactDOM.hydrateRoot(node, content)
+  if (!node) return new gleam.Error()
+  return new gleam.Ok(ReactDOM.hydrateRoot(node, content))
 }
 
 export function createPortal(children, root) {
   const node = document.getElementById(root)
-  return ReactDOM.createPortal(children, node)
+  if (!node) return new gleam.Error()
+  return new gleam.Ok(ReactDOM.createPortal(children, node))
 }
 
 export function render(root, children) {
